0b49428e33ee87085d50ae536456bdf2a43701ff
Guy D <guydaane@gmail.com>
2024-10-07 17:29:57 +0300

    structure decided, api tested and implemented

---
+++ src/services/movieService.js
@@ -0,0 +1,13 @@
+import axios from 'axios'
+
+const key = '4170bf35f7a61b8012d65de6ad644b9b'
+const url = 'https://api.themoviedb.org/3/movie/popular'
+
+export const getMovies = async () => {
+  try {
+    const response = await axios.get(`${url}?api_key=${key}&page=1`)
+  } catch (error) {
+    console.error("Couldn't fetch movies:", error)
+    return []
+  }
+}
